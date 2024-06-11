0431a2b40ff6918eb5276fca7b23b74751d672a2
mayuresh-byte <70745530+mayuresh-byte@users.noreply.github.com>
2024-06-11 09:13:47 +0530

    Optimised docker file

---
+++ week-15-live-2/src/index.ts
@@ -0,0 +1,31 @@
+
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const client = new PrismaClient();
+
+console.log("Hi. Now I am changing the index.ts so the caching should not happen now");
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Healthy server"
+    })
+})
+
+app.post("/", async (req, res) => {
+    await client.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name
+        }
+    })
+
+    res.json({
+        message: "Done signing up!"
+    })
+})
+
+app.listen(3000);
